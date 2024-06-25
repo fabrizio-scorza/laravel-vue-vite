74708ae7ea794d8595b4422a64c34d90689d0ae3
fabrizio-scorza <fabrizio.a.scorza@gmail.com>
2024-06-25 19:30:27 +0200

    fix test component

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
         <h1 class="display-5 fw-bold">
             Welcome to Laravel+Bootstrap <i class="bi bi-box"></i>
         </h1>
-
+        <test-components></test-components>
         <p class="col-md-8 fs-4">
             This is a preset package with Bootstrap 5 views for laravel projects including laravel breeze/blade. It works from laravel 9.x to the latest release 11.x.
             You can also use bootstrap icons out of the box.
